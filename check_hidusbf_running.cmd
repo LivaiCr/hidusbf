6b193fd9d264703cead3910d36cc8c8c150c252e
LivaiCr <89748162+LivaiCr@users.noreply.github.com>
2021-08-29 21:42:41 +0200

    Update check_hidusbf_running.cmd

--- check_hidusbf_running.cmd
+++ check_hidusbf_running.cmd
@@ -1,6 +1,6 @@
-@echo off
+@echo on
 sc query hidusbf | find /I "RUNNING" > nul
 if not errorlevel 1 goto end
-echo Hidusbf is NOT running !!!  
-pause 
-:end
+echo Hidusbf is running !!!  
+continue
+:start
